5a9dd260df491a27fbdbfb6e86f39ee534182fba
Yashwanth Hari Krishnan <yashhy@gmail.com>
2018-03-25 10:11:18 +0530

    - sample app for angular2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: '#cit-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  twoWayBinding = 'HELLO ';
   title = 'app';
+  canDisplay = false;
+  private arr = [1, 2, 3, 4];
+
+  newFunction123(param) {
+    if (this.twoWayBinding === '123') {
+      this.canDisplay = true;
+    }
+  }
+
 }
